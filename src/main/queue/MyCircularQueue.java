2fcafc3bf7d488dec8b90ef3efdf2ec7c787a307
yeeun <yeeeun910@gmail.com>
2022-08-11 13:03:48 +0900

    queue

---
+++ src/main/queue/MyCircularQueue.java
@@ -0,0 +1,41 @@
+package main.queue;
+
+public class MyCircularQueue<T> implements IQueue<T>{
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void enqueue(T data) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
